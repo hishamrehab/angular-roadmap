50aa64833ba851d13d9dcefe93bac7f114853038
hishamrehab <hishamrehab30@gmail.com>
2025-12-30 19:08:45 +0200

    Legacy Angular Using ngFor & ngIf

--- 1-angular-essentials/src/app/app.component.ts
+++ 1-angular-essentials/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgFor , NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from '../dummy-users';
@@ -8,14 +8,14 @@ import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor , NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = "u1"; 
+  selectedUserId?: string; 
 
 
   get selectedUser() {
