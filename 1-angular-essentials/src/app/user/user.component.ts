7861932f546f214002331ef84ef5ffae7ee242b3
hishamrehab <hishamrehab30@gmail.com>
2025-12-30 18:15:03 +0200

    Outpitting List Content

--- 1-angular-essentials/src/app/user/user.component.ts
+++ 1-angular-essentials/src/app/user/user.component.ts
@@ -1,4 +1,16 @@
-import { Component , EventEmitter, Input , Output , output } from '@angular/core';
+import { Component , EventEmitter, Input , Output  } from '@angular/core';
+
+// type User = {
+//     id: string;
+//     avatar: string;
+//     name: string
+//   };
+
+interface User {
+    id: string;
+    avatar: string;
+    name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -8,17 +20,16 @@ import { Component , EventEmitter, Input , Output , output } from '@angular/core
 })
 
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  //  @Output() select = new EventEmitter();
-  select =  output<string>();
+  @Input({required: true}) user!: User;
+
+  @Output() select = new EventEmitter<string>();
    
+  
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
