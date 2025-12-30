3c08cdcc97de0ec0abe1109a40562af14a457ca5
hishamrehab <hishamrehab30@gmail.com>
2025-12-30 15:44:41 +0200

    Using output function

--- 1-angular-essentials/src/app/user/user.component.ts
+++ 1-angular-essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component , computed, Input , input } from '@angular/core';
+import { Component , EventEmitter, Input , Output , output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,19 +8,17 @@ import { Component , computed, Input , input } from '@angular/core';
 })
 
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input<string>("");
-  name = input.required<string>();
-  
-  imagePath = computed(() => {
-    return `assets/users/` + this.avatar();
-  });
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  //  @Output() select = new EventEmitter();
+  select =  output<string>();
    
-  // get imagePath() {
-  //   return `assets/users/` + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }
