3e33f162c9e2fca83592c0db55d7ee6faed85de7
hishamrehab <hishamrehab30@gmail.com>
2026-01-20 16:52:50 +0200

    Mirging to Angular Modules

--- 03- Investment Calculator/investment-calculator/src/app/app.component.ts
+++ 03- Investment Calculator/investment-calculator/src/app/app.component.ts
@@ -1,15 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    UserInputComponent, 
-    InvestmentResultsComponent
-  ],
   templateUrl: './app.component.html',
 })
 
--- 03- Investment Calculator/investment-calculator/src/app/investment-results/investment-results.component.ts
+++ 03- Investment Calculator/investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,8 @@
 import { InvestmentService } from './../investment.service';
-import { CurrencyPipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
